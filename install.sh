8779c705cfbb38eb645c4380c22eaf3d4968f41a
jaredjpruett <jaredjpruett@gmail.com>
2015-05-05 12:06:13 -0500

    installs to dist-packages instead of site-packages

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$(id -u)" != "0" ]; then
-	echo "This script must be run as root" 1>&2
+	echo "This script must be run as root." 1>&2
 	exit 1
 fi
 
-echo "Copying daemonizer to site-packages"
-cp -R "daemonizer" "/usr/local/lib/python2.7/site-packages"
+echo "Copying daemonizer to dist-packages..."
+cp -R "daemonizer" "/usr/local/lib/python2.7/dist-packages"
 echo "Done."
